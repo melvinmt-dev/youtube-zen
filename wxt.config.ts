04a39f9e7569b2cde2126d8b723091e11d173352
Melvin <melvinthankachan101@gmail.com>
2026-08-07 20:50:13 +0530

    Initial commit

---
+++ wxt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ['@wxt-dev/module-react'],
+  manifest:{
+    name: "YoutubeZen",
+    description: "YoutubeZen is an extension to remove distractions.",
+    version: "0.0.1",
+  }
+});
